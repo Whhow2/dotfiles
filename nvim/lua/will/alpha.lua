3b4adb10d23dd8734b10da385b6dd375dc0a1b4e
Will Howard <whhow2@gmail.com>
2023-07-01 21:58:21 -0400

    added alpha for nvim, fzf, project, file browser for telescope, doom emacs

---
+++ nvim/lua/will/alpha.lua
@@ -0,0 +1,50 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
+	[[        /\          /\          /\       ]],
+	[[     /\//\\/\    /\//\\/\    /\//\\/\    ]],
+	[[  /\//\\\///\\/\//\\\///\\/\//\\\///\\/\ ]],
+	[[ //\\\//\/\\///\\\//\/\\///\\\//\/\\///\\]],
+	[[ \\//\/                            \/\\//]],
+	[[  \/                                  \/ ]],
+	[[  /\                                  /\ ]],
+	[[ //\\  Neovim IDE with LSP and DAP   //\\]],
+	[[ \\//                                \\//]],
+	[[  \/                                  \/ ]],
+	[[  /\                                  /\ ]],
+	[[ //\\/\                            /\//\\]],
+	[[ \\///\\/\//\\\///\\/\//\\\///\\/\//\\\//]],
+	[[  \/\\///\\\//\/\\///\\\//\/\\///\\\//\/ ]],
+	[[      \/\\//\/    \/\\//\/    \/\\//\/   ]],
+	[[         \/          \/          \/      ]],
+}
+
+dashboard.section.buttons.val = {
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope project <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+local function footer()
+	-- Quote
+	local fortune = require("alpha.fortune")
+	local quote = table.concat(fortune(), "\n")
+
+	return quote
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.config.opts.noautocmd = true
+alpha.setup(dashboard.config)
---
+++ nvim/lua/will/dashboard.lua
@@ -0,0 +1,4 @@
+local dashboard = require("dashboard")
+dashboard.setup({})
+
+vim.g.dashboard_default_executive = "telescope"
